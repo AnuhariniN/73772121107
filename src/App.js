e6a9ed8f3477fdb3625787c30c5114085f4adf8f
Your Name <you@example.com>
2023-12-04 23:38:22 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+          
+        </p>
+        <h6>Name : Anuharini N</h6>
+        <h6>Department : Information Technology</h6>
+        <h6>Year : III</h6>
+        <h6>Register number : 73772121107</h6>
+        <h6>Area of Interest : Web Development</h6>
+      </header>
+    </div>
+  );
+}
+
+export default App;
